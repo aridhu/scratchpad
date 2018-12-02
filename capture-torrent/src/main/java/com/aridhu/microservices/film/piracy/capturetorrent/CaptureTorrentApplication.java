95fe69a7d859341a510ddbd3c130e8413496d767
Bala Sankar <bala@aridhu.com>
2018-12-02 09:15:13 -0500

    Microservice to capture torrent

---
+++ capture-torrent/src/main/java/com/aridhu/microservices/film/piracy/capturetorrent/CaptureTorrentApplication.java
@@ -0,0 +1,12 @@
+package com.aridhu.microservices.film.piracy.capturetorrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureTorrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureTorrentApplication.class, args);
+	}
+}
